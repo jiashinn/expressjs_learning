ac526484b7bc95097c4a765808951ac19e8a830e
jiashinn <jiashin92@gmail.com>
2024-12-09 11:06:21 +0100

    Day 3: CRUD api completed and fetch api from fontend

--- router/posts.js
+++ router/posts.js
@@ -1,7 +1,14 @@
 import express from "express";
-const router = express();
+import {
+  getPost,
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+} from "../controller/postController.js";
+const router = express.Router();
 
-const posts = [
+let posts = [
   { id: 1, title: "Post 1" },
   { id: 2, title: "Post 2" },
   { id: 3, title: "Post 3" },
@@ -9,45 +16,20 @@ const posts = [
 
 // Get all posts
 
-router.get("/", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-
-  res.status(200).json(posts);
-});
+router.get("/", getPosts);
 
 // Get single post
 
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-
-  if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `A post with the id of ${id} was not found.` });
-  }
-
-  res.status(200).json(post);
-});
+router.get("/:id", getPost);
 
 //Create new post
-router.post("/", (req, res) => {
-  const newPost = {
-    id: posts.length + 1,
-    title: req.body.title,
-  };
+router.post("/", createPost);
 
-  if (!newPost.title) {
-    return res
-      .status(400)
-      .json({ msg: `Title cannot be empty, please try again` });
-  }
-  posts.push(newPost);
-  res.status(201).json(posts);
-});
+//Update post
+router.put("/:id", updatePost);
+
+// delete post
+
+router.delete("/:id", deletePost);
 
 export default router;
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import posts from "./router/posts.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
 const port = process.env.PORT;
 
 const app = express();
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //setup static folder
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/api/posts", posts);
+
+app.use(notFound);
+
+//Error handler
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server is running on port ${port} `));
